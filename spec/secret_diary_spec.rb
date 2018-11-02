e9a30ce0e4f61682b4b697457319d7647d9a99f0
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-02 16:44:50 +0000

    Last test and solution

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -2,14 +2,14 @@ require 'SecretDiary'
 # THE PASS CLASS
 describe Security do
 	context 'Key for the dairy is locked' do
-		it 'locked acces to diary' do
+		it 'locked' do
 			secret = Security.new
 			expect(secret.locked).to eq true
 		end
 	end
 
 	context 'Key for the dairy is unlocked' do
-		it 'unlocked acces to diary' do
+		it 'unlocked' do
 			secret = Security.new
 			expect(secret.unlocked).to eq false
 		end
@@ -18,18 +18,28 @@ end
 # THE METHOD CLASS
 describe Diary do
 	context 'Calling method while is block' do
-		it 'Raise error when is lock' do
+		it 'error' do
 			error = Diary.new
 			#Open {} i
-			expect{error.add_entry error.get_entries}.to raise_error('Method locked')
+			expect{error.add_entry error.get_entries}.to raise_error('Method block')
 		end
 	end
 
 	context 'Writing into the diary' do
-		it 'Writing a entry to the diary' do
+		it 'Writing..' do
 			write = Diary.new
 			write.unlocked
 			expect(write.add_entry('Page 1')).to eq('Page 1')
+		end
+	end
+
+	context 'Blocking method again' do
+		it 'Blocking diary again' do
+			error = Diary.new
+			#Open {} i
+			error.unlocked
+			error.locked
+			expect{error.add_entry error.get_entries}.to raise_error('Method block')
 		end
 	end
 end
