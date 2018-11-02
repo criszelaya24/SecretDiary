4d9395054d86ffabad597f08f8e402d79c35301b
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-02 11:23:21 +0000

    Adding second test, blocking method

--- spec/secret_diary_spec.rb
+++ spec/secret_diary_spec.rb
@@ -6,4 +6,11 @@ describe SecretDiary do
 			expect(secret.locked).to eq true
 		end
 	end
+
+	context 'Calling method while there are blocks' do
+		it 'error' do
+			error = SecretDiary.new
+			expect(secret.add_entry).to raise_error 'Method block'
+		end
+	end
 end
